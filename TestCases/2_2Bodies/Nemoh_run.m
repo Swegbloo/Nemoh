0fc58af1dd5ee868464f5b4931e031c6151f42e4
Swagoto Surjo Dutta <150083060+Swegbloo@users.noreply.github.com>
2024-03-24 02:28:04 +0530

    23032024

--- TestCases/2_2Bodies/Nemoh_run.m
+++ TestCases/2_2Bodies/Nemoh_run.m
@@ -5,4 +5,5 @@ w = 0.01:0.1:4;
 dir = 0;
 depth = 0;
 
-[~,~,A,B,Fe] = Nemoh(w, dir, depth);
+[A,B,Fe] = Nemoh('C:\Users\Swegbloo\Documents\GitHub\Nemoh\TestCases\2_2Bodies',1,0);
+save("A,B,Fe");
---
+++ matlabRoutines/cylinder/matcylrun.m
@@ -0,0 +1,9 @@
+clc
+clear all
+close all
+nfreq = 50;
+w = linspace(2.73, 8.86, nfreq);
+dir = 0;
+depth = 1;
+[A,B,Fe] = Nemoh(w, dir, depth);
+save('Nemohresult_test','A','B','Fe','w');
